d85cd3527bf1c91da37d5538be5303ef0b82ba0a
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-01-11 15:45:42 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,80 +1,51 @@
-// // var container_all = document.createElement("div");
+var result = fetch("https://memin.io/public/api/v2/users")
+.then(result => {
+    return result.json()
+}).then(data => {
+    data.data.forEach(function(element){
+      console.log(element.name);
 
-// const container_root = document.getElementById("root");
-// container_root.classList.add("row");
+      /*CAPTURO*/
+      let tableBody = document.getElementById('table_body');
 
-// const container_leftShowUser = document.createElement("div");
-// container_root.appendChild(container_leftShowUser);
 
-// container_leftShowUser.classList.add("col-md-6");
-// const cardShowUser = document.createElement("div");
+      /* CREO */
 
-// cardShowUser.classList.add("card");
-// container_leftShowUser.appendChild(cardShowUser);
+      let trRow = document.createElement('tr'); /* CREO LA FILA */
+      tableBody.appendChild(trRow);  /* agrego a la tabla la fila ROW */
 
-//p
-let p = document.createElement("p");
-p.innerHTML = "Users settings";
-cardShowUser.appendChild(p);
 
-var resultadoDePeticionServerAppi = fetch("https://memin.io/public/api/users")
-  .then((resultado) => {
-    return resultado.json();
-  })
-  .then((data) => {
-    data.forEach(function (element) {
-      //   const container_root = document.getElementById("root");
-      //   container_root.classList.add("row");
-      //   const container_leftShowUser = document.createElement("div");
-      //   container_root.appendChild(container_leftShowUser);
-      //   container_leftShowUser.classList.add("col-md-6");
-      //   const cardShowUser = document.createElement("div");
-      //   cardShowUser.classList.add("card");
-      //   container_leftShowUser.appendChild(cardShowUser);
-      //   //p
-      //   let p = document.createElement("p");
-      //   p.innerHTML = "Users settings";
-      //   cardShowUser.appendChild(p);
-    });
+      /* ID */
+      let tdId = document.createElement('td');
+      let idElement = element.id;
+      tdId.innerHTML = idElement;
+      trRow.appendChild(tdId);
+
+
+      /*NAME */
+
+      let tdName = document.createComment('td');
+      let nameElement = element.name;
+      tdName.innerHTML = nameElement;
+      trRow.appendChild(tdName);
+
+        // let tdCell = document.createElement('td'); /* agrego a la fila la celda */ 
+        // trRow.appendChild(tdCell);
+  
+        // let nameElement = element.name; /* capturo en una variable el nombre */
+        // tdCell.innerHTML = nameElement; /* agrego a la celda el nombre */
+
+
+        // /*MAIL */ 
+        // let emailElement = element.email;
+        // trRow;
+        // trRow.appendChild(tdCell);
+  
+        // tdCell.innerHTML = emailElement;
+
+     
+
+
+        
+    })
   });
-
-// var root = document.getElementById("root");
-
-// var result = fetch("https://memin.io/public/api/users")
-//   .then((result) => {
-//     return result.json();
-//   })
-//   .then((data) => {
-//     data.forEach(function (element) {
-//       let col = document.createElement("div");
-//       col.classList.add("col-md-3");
-//       root.appendChild(col);
-
-//       let card = document.createElement("div");
-//       card.classList.add("card");
-//       col.appendChild(card);
-
-//       let card_body = document.createElement("div");
-//       card_body.classList.add("card-body");
-//       card.appendChild(card_body);
-
-//       let p = document.createElement("p");
-//       p.innerText = element.name;
-//       card_body.appendChild(p);
-
-//       let button = document.createElement("button");
-//       button.classList.add("btn", "btn-danger");
-//       button.innerText = "Eliminar";
-//       card_body.appendChild(button);
-
-//       let button_actualizar = document.createElement("button");
-//       button_actualizar.classList.add("btn", "btn-warning");
-//       button_actualizar.innerText = "Actualizar";
-//       card_body.appendChild(button_actualizar);
-
-//       let button_detalles = document.createElement("button");
-//       button_detalles.classList.add("btn", "btn-info");
-//       button_detalles.innerText = "Ver detalles";
-//       card_body.appendChild(button_detalles);
-//     });
-//   });
